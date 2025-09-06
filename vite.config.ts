c511b454e53721882c9e5e5d82f707f063c7de62
nvminh155 <minhnv155@gmail.com>
2025-09-06 23:08:53 +0700

    remove mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import mkcert from "vite-plugin-mkcert"
+// import mkcert from "vite-plugin-mkcert"
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: 'assemble-sitecore.com',
   },
-  plugins: [react(), tailwindcss(), mkcert()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
